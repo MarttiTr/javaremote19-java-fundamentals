9756bb25d23ec301beb99c71f5f75017b73610ec
Martti Triksberg <triksberg.martti@gmail.com>
2022-09-14 19:24:40 +0300

    Helloworld added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,13 @@
+/**
+ * the first hello world prorgram
+ *
+ * @author Martti Triksberg
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!") ;
+
+
+    }
 }
