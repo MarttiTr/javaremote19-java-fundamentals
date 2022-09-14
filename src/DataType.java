823ec4a4367511fe9fc9c663eef6c51a0fd81d62
Martti Triksberg <triksberg.martti@gmail.com>
2022-09-14 20:05:22 +0300

    Added datatypes

---
+++ src/DataType.java
@@ -0,0 +1,29 @@
+/**
+ * Examples of primitive datatypes
+ *
+ * @author Martti Triksberg
+ */
+
+public class DataType {
+    int a = 1;
+    long longNumber = 18298189891289812L;
+    short shortNumber = -32000;
+    float decimalNumber = 2.45f;
+    double doubleNumber = 3.456d;
+
+    //character
+    char alpha = 'f';
+
+    //True/false
+    boolean hadLunch = true;  //had lunch?
+    boolean isHoliday = false;
+
+    int 1; //0
+
+    float x; //0.00f
+
+    char animal; //''
+
+    boolean isClassTomorrow; //false
+
+}
