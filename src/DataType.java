86473f96ff7ce4b818657d6e6d792a853e6e8b98
Martti Triksberg <triksberg.martti@gmail.com>
2022-09-14 20:54:53 +0300

    Added datatypes

--- src/DataType.java
+++ src/DataType.java
@@ -1,10 +1,12 @@
 /**
- * Examples of primitive datatypes
+ * Examples of primitive and non-primitive datatypes
  *
  * @author Martti Triksberg
  */
 
 public class DataType {
+    //Numbers
+    //Primitive datatypes
     int a = 1;
     long longNumber = 18298189891289812L;
     short shortNumber = -32000;
@@ -18,12 +20,60 @@ public class DataType {
     boolean hadLunch = true;  //had lunch?
     boolean isHoliday = false;
 
-    int 1; //0
+    int h; //0
 
     float x; //0.00f
 
     char animal; //''
 
     boolean isClassTomorrow; //false
+    
+    
+    //Non-primitive datatypes:
+    
+    String word = "I am a developer!";
+    
+    Character random = 't';
+    
+    Integer number = 5;
+    
+    Float numberSecond = 2.45F;
+    
+    Integer randomNUmber; //null
+
+    //Multideclaration
+
+    int numberOne, numberTwo, numberThree;
+    String fruit = "apple", vegetanle = "pumpkin";
+
+
+    private static String CAR = "Toyota"; //If private cant be used outside of this class.
+
+    public static String FOOD = "Cake";  //Same string can be used in other classes aswell.
+
+    public static void main(String[] args) {
+        int k = 0;
+        Integer y = null;
+
+        System.out.println(k);
+        System.out.println(y);
+
+
+        //Variable name can start with _, $ or letter
+
+        String _color = "black";
+
+        int $hexaNumber = 0x2A;
+
+        float exam12_mark = 1.332f;
+
+        String className = "sda";
+
+        System.out.println(CAR);
+        System.out.println(FOOD);
+
+        //Instance variables, class variables and local variables
+    }
+    
 
 }
