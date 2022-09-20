36cce506c489f5af9fd0276379eb395cada209f8
Martti Triksberg <triksberg.martti@gmail.com>
2022-09-20 15:16:32 +0300

    Stement homework added to feature/homework branch

---
+++ src/Homework2.java
@@ -0,0 +1,16 @@
+public class Homework2 {
+    public static void main(String[] args) {
+
+        int temp;
+        int x = 15;
+        int y = 25;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x = " + x + " and y = " + y);
+
+
+    }
+}
